7344704e95e3c5af9e61f073559a5ac394925e46
azizsvg <s5720554@bournemouth.ac.uk>
2024-11-20 22:04:41 +0000

    Updated restock.py with further functionality

--- restock.py
+++ restock.py
@@ -1,9 +1,13 @@
 def restock_inventory(available_items, inventory_records, current_day):
+    restocked_units = 0
     #Ensuring that it ONLY restock every weeks
     if current_day % 7 == 0:
+        #Displays how much units will be used to replenish the stock, at the start of each week.
+        restocked_units = 2000 - available_items
         #restocking amount set to 2000 items
-        available_items = 2000
+        available_items += restocked_units
         #Adding the details such as the current day, sold item, available items, and available units
-        inventory_records.append((current_day, 0, available_items, available_items))
-
+        inventory_records.append((current_day, 0, restocked_units, available_items))
+        
     return available_items
+
