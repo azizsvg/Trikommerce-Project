02395fdd6e9005a3055e899b1ab380239522a8f4
azizsvg <s5720554@bournemouth.ac.uk>
2024-11-20 23:51:29 +0000

    Carrying test to see if it display properly.

--- restock.py
+++ restock.py
@@ -1,12 +1,13 @@
 def restock_inventory(available_items, inventory_records, current_day):
-    restocked_units = 0
-    #Ensuring that it ONLY restock every weeks
+
+    #The If statement checks if current_day is a multiple of 7
     if current_day % 7 == 0:
-        #Displays how much units will be used to replenish the stock, at the start of each week.
+        #This equation shows the amount of items used to replenish the available_items to 2000
         restocked_units = 2000 - available_items
-        #restocking amount set to 2000 items
-        available_items += restocked_units
-        #Adding the details such as the current day, sold item, available items, and available units
+
+        #Available_item is set to 2000
+        available_items = 2000
+        #The data output on restocking day will be, the restocked units, and the available items.
         inventory_records.append((current_day, 0, restocked_units, available_items))
         
     return available_items
