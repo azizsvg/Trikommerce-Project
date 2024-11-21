e95b8bf955f028ffb6e930121a618e38d267be90
azizsvg <s5720554@bournemouth.ac.uk>
2024-11-21 01:01:27 +0000

    Using Append() to add the new rule for day 0

--- restock.py
+++ restock.py
@@ -1,10 +1,11 @@
 def restock_inventory(available_items, inventory_records, current_day):
     
     if current_day == 0:
-        #Ensuring both available and restocked items are set to 2000, due to it being the first day
+        #Ensuring both available items and restocked items are set to 2000, due to it being the first day and restocking day.
         available_items = 2000
         restocked_units =  2000
-        
+        inventory_records.append((current_day, 0, restocked_units, available_items))
+
 
     #The If statement checks if current_day is a multiple of 7
     if current_day % 7 == 0:
