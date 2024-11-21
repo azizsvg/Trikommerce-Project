5b64dd5a2952304a19f59b52df8298596ca6be14
azizsvg <s5720554@bournemouth.ac.uk>
2024-11-21 01:23:47 +0000

    Issue fixed, Restock.py runs properly.

--- restock.py
+++ restock.py
@@ -7,14 +7,14 @@ def restock_inventory(available_items, inventory_records, current_day):
         inventory_records.append((current_day, 0, restocked_units, available_items))
 
 
-    #The If statement checks if current_day is a multiple of 7
+    #The If statement checks if the current_day is a multiple of 7
     if current_day % 7 == 0:
         #This equation shows the amount of items used to replenish the available_items to 2000
         restocked_units = 2000 - available_items
 
         #Available_item is set to 2000
         available_items = 2000
-        #The data output on restocking day will be, the restocked units, and the available items.
+        #The data's output on restocking day, will be: the restocked units and the available items.
         inventory_records.append((current_day, 0, restocked_units, available_items))
         
     return available_items
