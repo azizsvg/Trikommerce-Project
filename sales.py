d476eb42f8a2128fb744160e5b209e97ba99c1d3
azizsvg <s5720554@bournemouth.ac.uk>
2024-11-21 01:27:33 +0000

    Sales.PY is fully functional.

--- sales.py
+++ sales.py
@@ -1,11 +1,11 @@
 import random
 
 def daily_sales(available_items, inventory_records, current_day):
-    #If current_day is NOT a multiple of 7
+#If current_day is NOT a multiple of 7
     if current_day % 7 != 0:
-        #Picks a number between 0 and 200
+#Picks a number between 0 and 200
         sold_unit = random.randint(0, 200)
-        #basically works as available_item = available_item - sold_unit
+#Basically works as available_item = available_item - sold_unit
         available_items -= sold_unit
 
         inventory_records.append((current_day, sold_unit, 0, available_items))
