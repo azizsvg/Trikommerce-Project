957a409a3fd93b3b22bc8f5bf21ae463dcb87f5a
azizsvg <s5720554@bournemouth.ac.uk>
2024-11-21 00:32:14 +0000

    Modifying the operator of the if statement to the correct one.

--- sales.py
+++ sales.py
@@ -2,7 +2,7 @@ import random
 
 def daily_sales(available_items, inventory_records, current_day):
     #If current_day is NOT a multiple of 7
-    if current_day != 7 == 0:
+    if current_day % 7 != 0:
         #Picks a number between 0 and 200
         sold_unit = random.randint(0, 200)
         #basically works as available_item = available_item - sold_unit
